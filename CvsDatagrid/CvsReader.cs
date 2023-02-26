cef8c264f620e8caab7be597bdf266db08f98022
Etienne Hurand <informatique@etiennehurand.fr>
2023-02-26 21:59:30 +0100

    Basic reading of cvs file

---
+++ CvsDatagrid/CvsReader.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data;
+
+namespace CvsDatagrid
+{
+    public class CvsReader
+    {
+        public string FileName { get; set; }
+        public string[] Headers { get; private set; }
+        public List<string[]> Rows{ get; private set; }
+        public bool Error { get; private set; }
+        public CvsReader(string fileName)
+        {
+            FileName = fileName;
+            Error = false;
+            if (File.Exists(FileName))
+            {
+                List<string>? lines = null;
+                try
+                {
+                    lines = File.ReadLines(fileName).ToList();
+                }
+                catch (Exception ex)
+                {
+                    Error = true;
+                }
+                if ((!Error) && (lines != null) && (!string.IsNullOrEmpty(lines[0])))
+                {
+                    try
+                    {
+                        Headers = lines[0].Split(";");
+                    }
+                    catch (FormatException ex)
+                    {
+                        Error = true;
+                    }
+                }
+                // Continue with data rows
+                if (!Error)
+                {
+                    Rows = new List<string[]>();
+                    for (int i = 1; i < lines.Count; i++)
+                    {
+                        string[]? row = null;
+                        try
+                        {
+                            row = lines[i].Split(";");
+                        }
+                        catch (FormatException ex)
+                        {
+                            Error = true;
+                        }
+                        if ((!Error) && (row != null))
+                        {
+                            Rows.Add(row);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Error = true;
+            }
+        }
+    }
+}
---
+++ CvsDatagrid/VM/VM_Main.cs
@@ -0,0 +1,63 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Win32;
+using System.Data;
+
+namespace CvsDatagrid.VM
+{
+    public class VM_Main : ObservableRecipient
+    {
+        public ICommand Cmd_OpenFile { get; }
+        public DataTable TableData { get; private set; }
+        public VM_Main()
+        {
+            Cmd_OpenFile = new RelayCommand(OpenFile);
+        }
+
+        public void OpenFile()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (!string.IsNullOrEmpty(AppSettings.Default.InitialPath))
+            {
+                openFileDialog.InitialDirectory = AppSettings.Default.InitialPath;
+            }
+            else
+            {
+                openFileDialog.InitialDirectory = Environment.CurrentDirectory;
+            }
+            bool? result = openFileDialog.ShowDialog();
+            if (result == true)
+            {
+                string fileName = openFileDialog.FileName;
+                if (File.Exists(fileName))
+                {
+                    FileInfo fileInfo = new FileInfo(fileName);
+                    AppSettings.Default.InitialPath = fileInfo.DirectoryName;
+                    AppSettings.Default.Save();
+                    CvsReader cvsReader = new CvsReader(fileName);
+                    if (!cvsReader.Error)
+                    {
+                        TableData = new DataTable();
+                        foreach (string header in cvsReader.Headers)
+                        {
+                            TableData.Columns.Add(new DataColumn(header));
+                        }
+                        foreach (string[] row in cvsReader.Rows)
+                        {
+                            TableData.Rows.Add(row);
+                        }
+                        OnPropertyChanged(nameof(TableData));
+                    }
+                }
+            }
+
+        }
+    }
+}
